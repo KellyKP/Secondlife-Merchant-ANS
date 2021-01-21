1513cc73e45c841119cb463998fc3c513fee7f18
PeterStindberg <77799975+PeterStindberg@users.noreply.github.com>
2021-01-21 18:23:40 +0100

    Fixed issues with SQL statement
    Auto-increment fields can cause an issue under some dialects. With the changes, the auto-increment works fine now.

--- ans.php
+++ ans.php
@@ -1,38 +1,28 @@
-<?php 
+<?php
 
-    $con=mysql_connect("localhost", "Username", "Password") or die(mysql_error());
-    mysql_select_db("Database",$con) or die(mysql_error());
+$con=mysql_connect("localhost", "id15958800_slmarketplace", "-grWWdCrTaHoP5xx") or die(mysql_error());
+mysql_select_db("id15958800_marketplace",$con) or die(mysql_error());
 
- 	$tTransactionID = $_GET['TransactionID'];
-	$Timestamp = gmdate("Y-m-d\TH:i:s\Z",time());
- 	$tItemID = $_GET['ItemID'];
- 	$tItemName = $_GET['ItemName'];
- 	$tMerchantName = $_GET['MerchantName'];	
-	$tPayerName = $_GET['PayerName'];
-	$tPayerKey = $_GET['PayerKey'];
- 	$tReceiverName = $_GET['ReceiverName'];
- 	$tReceiverKey = $_GET['ReceiverKey'];
-	$tPaymentGross = $_GET['PaymentGross'];
- 	$tLocation = $_GET['Location'];
+$tTransactionID = $_GET['TransactionID'];
+$Timestamp = gmdate("M d Y H:i:s",mktime());
+$tItemID = $_GET['ItemID'];
+$tItemName = $_GET['ItemName'];
+$tMerchantName = $_GET['MerchantName'];
+$tPayerName = $_GET['PayerName'];
+$tPayerKey = $_GET['PayerKey'];
+$tReceiverName = $_GET['ReceiverName'];
+$tReceiverKey = $_GET['ReceiverKey'];
+$tPaymentGross = $_GET['PaymentGross'];
+$tLocation = $_GET['Location'];
 if ($tLocation != 'In-World')
-	$tLocation = 'Marketplace';
+$tLocation = 'Marketplace';
 
 if ($tPaymentGross != '0')
-{   
-$sql="INSERT INTO ANS VALUES ('','$Timestamp',
-'$tTransactionID',
-'$tItemID',
-'$tItemName',
-'$tMerchantName',
-'$tPayerName',
-'$tPayerKey',
-'$tReceiverName',
-'$tReceiverKey',
-'$tPaymentGross',
-'$tLocation'
-)";
-$result=mysql_query($sql) or die(mysql_error());
-}
+{
+  $sql="INSERT INTO ANS (Time, TransactionID, ItemID, ItemName, MerchantName, PayerName, PayerKey, ReceiverName, ReceiverKey, PaymentGross, Location) VALUES ('$Timestamp',
+    '$tTransactionID', '$tItemID', '$tItemName', '$tMerchantName', '$tPayerName', '$tPayerKey', '$tReceiverName', '$tReceiverKey', '$tPaymentGross', '$tLocation' );";
+    $result=mysql_query($sql) or die(mysql_error());
+  }
 
-mysql_close($con);
-?>
+  mysql_close($con);
+  ?>
